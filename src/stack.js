d6d12feb5f19b2b0fc26e4de063e8e762cb348fa
Richard <1021679588@qq.com>
2018-03-31 19:59:50 +0800

    add stack

---
+++ src/stack.js
@@ -0,0 +1,19 @@
+class Stack {
+  constructor() {
+    this.items = []
+  }
+  push(item) {
+    this.items.push(item)
+  }
+  pop() {
+    return this.items.pop()
+  }
+  clear() {
+    this.items = []
+  }
+  isEmpty() {
+    return this.items.length === 0
+  }
+}
+
+module.exports = Stack
